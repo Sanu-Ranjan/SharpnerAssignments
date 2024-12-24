137f484c21311f12574ba8476bf417557a3bb0a0
Sanu-Ranjan <ranjan.code33@gmail.com>
2024-12-24 16:32:07 +0530

    creatig elements to append

--- ExpenseTracker/index.js
+++ ExpenseTracker/index.js
@@ -1,5 +1,24 @@
 //handling form submission
+class Expense {
+  constructor(amt, description, category) {
+    this.amount = amt;
+    this.description = description;
+    this.category = category;
+  }
+}
+
 document.getElementById("expenseForm").addEventListener("submit", (event) => {
   event.preventDefault();
-  alert("working");
+  let entry = new Expense(
+    event.target.amount,
+    event.target.description,
+    event.target.category
+  );
+
+  let listItem = document.createElement("li");
+  listItem.textContent = `${entry.amount} - ${entry.category} - ${entry.description} `;
+  let deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete Entry";
+  let editBtn = document.createElement("button");
+  editBtn.textContent = "Edit Expense";
 });
