9309dfa87933c4f4ca308a1b19dda7a37ff82193
Sanu-Ranjan <ranjan.code33@gmail.com>
2024-12-24 17:16:33 +0530

    delete functionality added

--- ExpenseTracker/index.js
+++ ExpenseTracker/index.js
@@ -14,16 +14,26 @@ document.getElementById("expenseForm").addEventListener("submit", (event) => {
     event.target.description.value,
     event.target.category.value
   );
+
   let listItem = document.createElement("li");
   listItem.className = "expenseList";
   listItem.textContent = ` ${entry.amount} - ${entry.category} - ${entry.description} `;
+
   let deleteBtn = document.createElement("button");
   deleteBtn.className = "deleteEntry";
   deleteBtn.textContent = "Delete Entry";
+
   let editBtn = document.createElement("button");
   editBtn.className = "editEntry";
   editBtn.textContent = "Edit Expense";
+
   listItem.appendChild(deleteBtn);
   listItem.appendChild(editBtn);
+
   document.getElementById("entries").append(listItem);
+});
+
+document.getElementById("entries").addEventListener("click", (event) => {
+  console.log(event.target);
+  event.target.parentElement.remove();
 });
