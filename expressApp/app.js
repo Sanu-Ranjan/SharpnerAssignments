ea064f66f2954b4f05f2a5dc50ef8817f041eec3
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-08-01 02:39:14 +0530

    Question Routes

--- expressApp/app.js
+++ expressApp/app.js
@@ -4,17 +4,24 @@ const app = express();
 
 const port = 3000;
 
-app.use("/welcome", (req, res, next) => {
-  req.user = "Guest";
-  next();
+app.get("/orders", (req, res) => {
+  res.send(`<h1>Here is the list of all orders.</h1>`);
 });
 
-app.get("/welcome", (req, res) => {
-  res.send(`<h1> Welcome ${req.user} </h1>`);
+app.post("/orders", (req, res) => {
+  res.send(`<h1>A new order has been created.</h1>`);
+});
+
+app.get("/users", (req, res) => {
+  res.send(`<h1>Here is the list of all users.</h1>`);
+});
+
+app.post("/users", (req, res) => {
+  res.send(`<h1>A new user has been added.</h1>`);
 });
 
 app.listen(port, () => {
   console.log(
-    `Server is up and running on port ${port}! Ready to handle requests.`
+    `Server is up and running on http://localhost:${port}! Ready to handle requests.`
   );
 });
