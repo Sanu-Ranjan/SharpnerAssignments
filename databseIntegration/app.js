e28f7a8b704877842aab4b9bd788c69d365618be
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-08-06 18:50:41 +0530

    connect app to databse

---
+++ databseIntegration/app.js
@@ -0,0 +1,14 @@
+const app = require("express")();
+
+const mysql = require("mysql2");
+
+const port = 3000;
+
+mysql.createConnection({
+    host:localhost,
+    user:
+})
+
+app.listen(port, () => {
+  console.log(`listening on port:${port}`);
+});
