67a167f310424ed5e38244cab9d78d79f881d117
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-08-06 23:29:11 +0530

    refactor(config): use config and dotenv for secure DB credentials and port setup

--- databseIntegration/app.js
+++ databseIntegration/app.js
@@ -1,13 +1,27 @@
+require("dotenv").config();
+const config = require("config");
+
 const app = require("express")();
 
 const mysql = require("mysql2");
 
-const port = 3000;
+const port = config.get("port");
 
-mysql.createConnection({
-    host:localhost,
-    user:
-})
+const conection = mysql.createConnection({
+  host: config.get("db.host"),
+  port: config.get("db.port"),
+  user: config.get("db.user"),
+  password: config.get("db.password"),
+  database: config.get("db.database"),
+});
+
+conection.connect((err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(`connection has been created`);
+});
 
 app.listen(port, () => {
   console.log(`listening on port:${port}`);
