a7a06bd5480e2f3c16254ebf926e071fa20aa6e9
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-08-09 11:46:49 +0530

    full-stack-app Backend user schema added

--- full-stack-booking-App/backend/app.js
+++ full-stack-booking-App/backend/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
 const { db } = require("./config/dbConnect");
-
+require("./models/user");
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
 
 db.sync({ force: true }).then(() => {
   app.listen(port, () => {
---
+++ full-stack-booking-App/backend/models/user.js
@@ -0,0 +1,28 @@
+const { db } = require("../config/dbConnect");
+
+const { DataTypes } = require("sequelize");
+
+const Users = db.define("users", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+});
+
+module.exports.Users = Users;
