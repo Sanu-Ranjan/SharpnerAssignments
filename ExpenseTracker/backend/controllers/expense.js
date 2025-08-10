280e5188a9a288647fda74bcc4e8324ec5d4600a
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-08-10 23:20:12 +0530

    Expense Tracker Backend logic to edit expense added

--- ExpenseTracker/backend/controllers/expense.js
+++ ExpenseTracker/backend/controllers/expense.js
@@ -1,6 +1,6 @@
 const { Expense } = require("../models/expense");
 
-const { Op } = require("sequelize");
+const { Op, where } = require("sequelize");
 
 const dbErrorHandler = (err, res) => {
   res.status(500).json({
@@ -75,6 +75,44 @@ const deleteExpense = async (req, res) => {
   }
 };
 
+const editEntries = async (req, res) => {
+  const { id } = req.params;
+  const { amount, description, category } = req.body;
+
+  try {
+    const expense = await Expense.update(
+      {
+        amount: amount,
+        description: description,
+        category: category,
+      },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+
+    if (!expense)
+      return res.status(404).json({
+        success: false,
+        error: "No records found",
+        details: {
+          id: id,
+        },
+      });
+
+    res.status(200).json({
+      success: true,
+      data: expense,
+      mesage: "Expense Updated",
+    });
+  } catch (error) {
+    dbErrorHandler(error, res);
+  }
+};
+
 module.exports.getExpense = getExpense;
 module.exports.addExpense = addExpense;
 module.exports.deleteExpense = deleteExpense;
+module.exports.editEntries = editEntries;
--- ExpenseTracker/backend/routes/expense.js
+++ ExpenseTracker/backend/routes/expense.js
@@ -2,6 +2,7 @@ const {
   getExpense,
   addExpense,
   deleteExpense,
+  editEntries,
 } = require("../controllers/expense");
 
 const router = require("express").Router();
@@ -9,5 +10,5 @@ const router = require("express").Router();
 router.get("/", getExpense);
 router.post("/", addExpense);
 router.delete("/:id", deleteExpense);
-
+router.put("/:id", editEntries);
 module.exports.router = router;
