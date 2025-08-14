a726040565d2909639fea53a0cc5e14b8f1a9d4b
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-08-14 22:37:18 +0530

    Very basic Blog post page reading input from form

--- BlogPostClient/index.js
+++ BlogPostClient/index.js
@@ -1,1 +1,19 @@
 console.log("working");
+
+const post = document.getElementById("entries");
+
+const form = document.getElementById("blogForm");
+
+const content = document.getElementById("content");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const blog = {
+    title: formData.get("title"),
+    author: formData.get("author"),
+    content: formData.get("content"),
+  };
+  console.log(blog);
+});
