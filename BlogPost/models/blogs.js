a14e16e1fe2ef47c0e0c5559964b4e950fac4fab
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-08-14 20:29:19 +0530

    BlogPost API- Blogs model added

---
+++ BlogPost/models/blogs.js
@@ -0,0 +1,27 @@
+const { dbconnect } = require("../config/dbConnect");
+
+const { DataTypes } = require("sequelize");
+
+let Blogs = dbconnect.define("Blogs", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  author: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
+
+module.exports = {
+  Blogs,
+};
